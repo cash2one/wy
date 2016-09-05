e0d38a78ce747c547e57e16ed7427b5130650a9e
fenpan.lfp <linfenpan@corp.netease.com>
2016-09-05 19:36:20 +0800

    登录

---
+++ login/xyq/js/user_login.js
@@ -0,0 +1,134 @@
+/*
+ *
+ */
+
+ // TODO 登录成功后，写入 serverName: Cookie.write("cur_servername", encodeURIComponent($("server_name").value));
+ // TODO 成功登录前，需要删除 Cookie.dispose("is_user_login"); Cookie.dispose("login_user_nickname");
+ // TODO LoginRequiresCaptcha === true，才需要验证码，否则不需要
+
+// qrcode_login_init(CgiRootUrl, HttpsCgiRootUrl, {size: [156, 156]});
+
+// 重写 message 的机制
+var old_qrcode_login_message = QRCodeLogin.prototype.message;
+QRCodeLogin.implement('message', function(msg){
+	var $qrcode = $('qrcode');
+	if (msg.indexOf('超时') >= 0) {
+    var $refresh = new Element('div', {
+      html: '<div class="refreshBG"></div><div class="refreshCnt"><span class="text">二维码已失效</span><a href="javascript:qrcode_manual_update();" class="refreshBt">点击刷新</a></div>'
+    });
+    $qrcode.appendChild($refresh);
+	} else {
+		$qrcode.set('html', msg);
+	}
+	this.has_qrcode = false;
+});
+
+function set_server_info() {
+	var area_name   = htmlEncode(decodeURIComponent(getPara("area_name")));
+	var server_name = htmlEncode(decodeURIComponent(getPara("server_name")));
+	var server_id   = htmlEncode(getPara("server_id"));
+	var area_id     = htmlEncode(getPara("area_id"));
+	var equip_id    = htmlEncode(getPara("equip_id"));
+
+	var server_name_info = '';
+	if(area_name) {
+		server_name_info = area_name + "&raquo;";
+	}
+	if(server_name) {
+		server_name_info += server_name;
+	}
+
+	$("server_name_info").set('html', server_name_info);
+	$("server_name").value     = server_name;
+	$("server_id").value       = server_id;
+	$("area_id").value         = area_id;
+	$("area_name").value         = area_name;
+	$("equip_id").value        = equip_id;
+}
+
+// 匿名访问
+function anonymous_search() {
+    Cookie.dispose("is_user_login");
+    Cookie.dispose("login_user_nickname");
+
+    var serverId = $("server_id").value;
+    var serverName = $("server_name").value;
+    var nextUrl = getPara('return_url')
+    if (!nextUrl && StaticFileConfig["is_using"]) {
+      nextUrl = StaticFileConfig["res_root"] + "/" + serverId + "/buy_equip_list/equip_list1.html";
+    } else {
+      nextUrl = decodeURIComponent(nextUrl);
+    }
+
+    var obj_form = document.anonymous_form;
+    Cookie.write("cur_servername", encodeURIComponent(serverName));
+
+    submit_by_form({
+      url: obj_form.action,
+      method: obj_form.method,
+      data: {
+        act: 'do_anon_auth',
+        server_id: serverId,
+        server_name: serverName,
+        next_url: nextUrl
+      }
+    });
+}
+
+function submit_by_form(options) {
+  options = Object.merge({ method: 'post', url: '/', data: {} }, options || {});
+
+  var data = options.data;
+  var form = document.createElement('form');
+  var method = options.method.toLowerCase();
+  form.setAttribute('method', method);
+  form.setAttribute('action', options.url);
+  options.target && form.setAttribute('target', options.target);
+
+  Object.each(data, function(value, key) {
+    var input = document.createElement('input');
+    input.setAttribute('id', key);
+    input.setAttribute('name', key);
+    input.value = value;
+    form.appendChild(input);
+  });
+
+  document.getElementsByTagName('body')[0].appendChild(form);
+
+  form.submit();
+  // alert(form.innerHTML)
+}
+
+
+function init_page() {
+	set_server_info();
+}
+
+function login_tab_init() {
+	qrcode_login_init(CgiRootUrl, HttpsCgiRootUrl, {size: [156, 156]});
+
+	// tab 切换
+  var tabTimer;
+	$$('.loginTab .item').addEvent('mouseenter', function(e){
+		var el = this;
+    if (el.hasClass('active')) {
+      return;
+    }
+
+    // 防止过于灵活
+    clearTimeout(tabTimer);
+    tabTimer = setTimeout(function() {
+      el.addClass('active')
+  			.getSiblings('.active')
+  			.removeClass('active');
+
+  		var index = el.get('data-tab');
+  		$$('.loginTabContent .tabContent').each(function($el) {
+  			$el.setStyle('display', $el.hasClass('tabContent' + index) ? '' : 'none');
+  		});
+
+      var fn = el.get('data-event-active');
+  		fn && window[fn] && window[fn]();
+    }, 200);
+	});
+}
