e46c112dfc2e45c5d36f9f7f625bbbceefdcdcbe
fenpan.lfp <linfenpan@corp.netease.com>
2016-11-24 10:00:41 +0800

    light7 初步学习

---
+++ light7/README.md
@@ -0,0 +1,5 @@
+先安装环境 ``` npm install lfp-mock-web -gd ```
+
+之后在本目录运行命令: ``` npm install ``` 和 `` lfp-mock-web -p=4444 --open ```
+
+访问: /index.html
---
+++ light7/router.js
@@ -0,0 +1,26 @@
+// router.js
+'use strict';
+
+const Mock = require('lfp-mock-web');
+const nunjucksBuilder = Mock.nunjucksBuilder;
+const staticResource = Mock.staticResource;
+
+module.exports = {
+  // 配置 get 请求，访问规则为: /pat/xxx.html
+  'GET /:page.html' (req, res, next) {
+    nunjucksBuilder.build(req.params.page + '.html', res, {
+      // 默认数据
+    });
+  },
+  // 配置静态资源访问规则
+  'GET *.:ext' (req, res, next) {
+    const ext = req.params.ext.toLowerCase();
+    if (['js', 'css', 'png', 'jpg', 'jpeg', 'gif', 'bmp', 'txt', 'less', 'scss'].indexOf(ext) >= 0) {
+      // 静态资源转发到本地临时目录
+      staticResource.query(req, res, next);
+    } else {
+      // 继续运行之后的规则
+      next();
+    }
+  }
+};
