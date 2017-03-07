b5ebb75132fa4600488f0f1d557ac0f154909bb2
fenpan.lfp <linfenpan@corp.netease.com>
2017-03-07 14:23:04 +0800

    删除代码

--- error-log/lib/md5.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const crypto = require('crypto');
-
-module.exports = function(str) {
-  const md5sum = crypto.createHash('md5');
-  md5sum.update(str);
-  return md5sum.digest('hex');
-};
